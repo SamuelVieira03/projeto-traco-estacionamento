28f4649a188b64b72075bbd523d637e77cd6fb28
SamuelVieira03 <82007689+SamuelVieira03@users.noreply.github.com>
2022-04-15 23:27:41 -0300

    index from visuals - view documents

--- index.js
+++ index.js
@@ -1,32 +1,204 @@
 
-import {CadastroComponent} from "./controller/cadastro.js";
-import { FaturamentoComponent } from "./controller/faturamento.js";
-import { ListaClienteComponent } from "./controller/lista-Clientes.js";
-import{view} from "./view/index.js"
+//Pagina inicial do index
+const getIndex = () => {
+    const main = document.getElementById('root')
+    const dadosHtml = `
+      <section class="index">
+        <h1 class="index__item">Bem vindo ao modulo 3 newDev() Traco!</h1>
+        <a class="index__link" href="../checkin.html"><img src="../assets/img/traco-principal.png" /></a>
+      </section>
+      `
+    main.innerHTML = dadosHtml
+}
+//Spinner para mostrar o loading da página
+const getSpinner = () => {
+    const main = document.getElementById('root')
+    const dadosHtml = `
+    <section class="container">
+        <div id="spinner"></div>
+    </section>
+      `
+    main.innerHTML = dadosHtml
 
-view.getSpinner()
-    setTimeout(() => {
-        view.getIndex();
-    }, 600)
+}
+//Puxar a ficha cadastral do HTML para o Javascript
+  const getCadastroHtml = () =>{
+  const main = document.getElementById('root')
+    const dadosHtml = `
+            <form class="cadastro" id="formulario">
+                <h1 class="cadastro__titulo">Novo Cliente</h1>
+                <label>Nome do Cliente</label>
+                <input id="name" type="text" placeholder="Digite o seu nome">
+                <label>Modelo</label>
+                <input id="modelo" type="text" placeholder="Digite o modelo do veículo">
+                <label>Tipo</label>
+                <select id="tipo" type="text" placeholder="Digite o tipo do veículo">
+                    <option value="1">Carro</option>
+                    <option value="0">Moto</option>
+                </select>                
+                <label>Placa</label>
+                <input id="placa" type="text" placeholder="Digite a placa do veículo">
+                <label>Observações</label>
+                <input id="observacoes" type="text" placeholder="Digite a observação:">
+                <div class="cadastro__item">
+                    <button class="button" id="cancelar" type="button">Cancelar</button>
+                    <button class="button" id="salvar" type="submit">Salvar</button>
+                </div>
+            </form> `
 
-const link = document.getElementById('link')
+    main.innerHTML = dadosHtml
+}
+//Puxar a lista de Clientes do HTML para o JavaScript
+        const getListaClientesHtml = () => {  
+        const main = document.getElementById('root')
+        const dadosHtml = `
+        <section class="lista-cliente">
+            <h1 class="lista-cliente__title">Lista de Clientes</h1>
+            <table class="lista-cliente__table" id="tbody">
+            <tr>
+                <th class="none">Cliente</th>
+                <th>Modelo</th>
+                <th>Placa</th>
+                <th class="none">Tipo</th>
+                <th class="none">Observações</th>
+                <th><a>Novo</a></th>
+            </tr>
+            </table>
+        </section> `
 
-link.addEventListener('click', (event) => {
-    const option = event.target.innerText
+    main.innerHTML = dadosHtml
 
-    switch(option){
-        case "Cadastro":
-            CadastroComponent();
-            break
-        case "Clientes":
-            ListaClienteComponent();
-            break
-        case "Checkin": 
-            window.location.href = "./checkin.html"
-            break
-        case "Faturamento":
-            FaturamentoComponent();
-            break;
-        default : view.getIndex()
+   
+ }
+
+ //Voltar a pagina inicial
+    const getAtualizaHtml = () =>{
+    const main = document.getElementById('root')
+    const dadosHtml = `
+            <form class="cadastro" id="formulario">
+                <h1 class="cadastro__titulo">Editar Cliente</h1>
+                <label>Nome do Cliente</label>
+                <input id="name" type="text" placeholder="Digite o seu nome">
+                <label>Modelo</label>
+                <input id="modelo" type="text" placeholder="Digite o modelo do veículo">
+                <label>Tipo</label>
+                <select id="tipo" type="text" placeholder="Digite o tipo do veículo">
+                    <option value="1">Carro</option>
+                    <option value="0">Moto</option>
+                </select>                
+                <label>Placa</label>
+                <input id="placa" type="text" placeholder="Digite a placa do veículo">
+                <label>Observações</label>
+                <input id="observacoes" type="text" placeholder="Digite a observação:">
+                <div class="cadastro__item">
+                    <button class="button" id="cancelar" type="button">Cancelar</button>
+                    <button class="button" id="salvar" type="submit">Atualizar</button>
+                </div>
+            </form> `
+
+    main.innerHTML = dadosHtml
+}
+//Página de Checkin com as informações necessárias
+    const getCheckinHtml =()=>{
+      const main = document.getElementById('root')
+    const dadosHtml = `
+        <section class="checkin">
+            <h2 class="checkin__title">Lista de Clientes no Estacionamento</h2>
+            <table id="tbody" class="checkin__table">
+            <tr>
+                <th>Modelo</th>
+                <th>Placa</th>
+                <th>Opção</th>
+            </tr>
+            </table>
+            <div class="checkin__item">
+            <label>Placa</label>
+            <select class="checkin__input" id="select"></select>
+            <a type="button" id="adicionar-novo" class="checkin__item__link">Adicionar Novo</a>
+            <div class="checkin__button">
+                <button class="button" id="checkin">Checkin</button>
+            </div>
+            </div>
+        </section>
+  `
+
+    main.innerHTML = dadosHtml
+
     }
-})
+// Página de checkout com informações e valores
+    const getCheckoutHtml = () =>{
+      const main = document.getElementById('root')
+    const dadosHtml = `
+          <section class="checkout">
+            <h2 class="checkout__title">Dados do cliente</h2>
+            <table class="checkout__table">
+                <tr>
+                    <th>Cliente</th>
+                    <th>Modelo</th>
+                    <th>Placa</th>
+                </tr>
+                <tr id="tbody"></tr>
+            </table>
+            <div class="checkout__item">
+            <label>Total de Horas</label>
+            <input
+                id="totalHora"
+                class="input"
+                type="text"
+                placeholder="Total de Horas"
+                disabled
+            />
+            </div>
+            <div class="checkout__item">
+            <label>Valor a pagar</label>
+            <input
+                id="valorPagar"
+                class="input"
+                type="text"
+                placeholder="Valor a pagar"
+                disabled
+            />
+            </div>
+            <div class="checkout__button">
+                <button class="button" id="finalizar">Finalizar</button>
+            </div>
+          </section>
+      `
+    main.innerHTML = dadosHtml
+    }
+//Faturamento e contabilização de carros
+    const getFaturamentoHtml = ()=>{
+        const main = document.getElementById('root')
+    const dadosHtml = `
+          <section class="faturamento">
+            <h1 class="faturamento__title">Lista de Faturamento</h1>
+            <table class="faturamento__table">
+                <thead>
+                <tr>
+                    <th>Qtd Veiculos</th>
+                    <th>Total</th>
+                    <th>Dia</th>
+                </tr>
+                </thead>
+                <tbody id="tbody"></tbody>
+            </table>
+            <div>
+                <div class="faturamento__grafico" id="piechart_3d"></div>
+            </div>
+          </section>
+      `
+    main.innerHTML = dadosHtml
+    }
+
+    //exportação dos conteudos para os outros modulos
+ export const view = {
+    getListaClientesHtml,
+    getCadastroHtml,
+    getAtualizaHtml,
+    getCheckinHtml,
+    getCheckoutHtml,
+    getFaturamentoHtml,
+    getSpinner,
+    getIndex
+    
+ }
